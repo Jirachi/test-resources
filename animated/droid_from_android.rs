fa58e8a97efa3ba2e98c09a957ce858ba9f412fa
XpLoDWilD <xplodgui@gmail.com>
2012-10-23 21:13:32 +0200

    Update droid animation resource

--- animated/droid_from_android.rs
+++ animated/droid_from_android.rs
@@ -15,6 +15,62 @@
 				"end_col":2,
 				"start_line":0,
 				"end_line":0
+			},
+			{
+				"name":"walk",
+				"start_col":0,
+				"end_col":2,
+				"start_line":1,
+				"end_line":1
+			},
+			{
+				"name":"propulsion_walk",
+				"start_col":0,
+				"end_col":2,
+				"start_line":2,
+				"end_line":2
+			},
+			{
+				"name":"hit",
+				"start_col":0,
+				"end_col":2,
+				"start_line":3,
+				"end_line":3
+			},
+			{
+				"name":"punch",
+				"start_col":0,
+				"end_col":2,
+				"start_line":4,
+				"end_line":4
+			},
+			{
+				"name":"kick",
+				"start_col":0,
+				"end_col":2,
+				"start_line":5,
+				"end_line":5
+			},
+			{
+				"name":"propulsion_punch",
+				"start_col":0,
+				"end_col":2,
+				"start_line":6,
+				"end_line":6
+			},
+			{
+				"name":"propulsion_kick",
+				"start_col":0,
+				"end_col":2,
+				"start_line":7,
+				"end_line":7
+			},
+			{
+				"name":"die",
+				"start_col":0,
+				"end_col":2,
+				"start_line":8,
+				"end_line":8
 			}
 		]
 	}
